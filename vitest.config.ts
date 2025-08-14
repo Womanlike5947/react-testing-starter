2977794f47ad7366fc495f76790fb992d56c4b43
holly bishop <hollybish10@hotmail.co.uk>
2025-08-14 12:02:41 +0100

    2.4 - Simplifying Test Setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,9 @@ import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
     test:{
-        environment:'jsdom'
+        environment:'jsdom',
+        globals:true,
+        // ⬇️ This shall run before every test file
+        setupFiles:'tests/setup.ts'
     }
 })
