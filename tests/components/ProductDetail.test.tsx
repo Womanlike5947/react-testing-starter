94b74fad1bdbc1c7a0354a250e445c0d8f0449f7
holly bishop <hollybish10@hotmail.co.uk>
2025-08-19 16:56:15 +0100

    3.4 - Exercise: Testing Data Fetching

--- tests/components/ProductDetail.test.tsx
+++ tests/components/ProductDetail.test.tsx
@@ -1,6 +1,35 @@
 import { test, expect, describe } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import ProductDetail from '../../src/components/ProductDetail';
+import { products } from '../mocks/data';
+import { http, HttpResponse } from 'msw';
+import { server } from '../mocks/server';
 
 describe('ProductDetail', () => {
-	test('should', () => {});
+	test('should render the list of products', async () => {
+		render(<ProductDetail productId={1} />);
+
+		expect(
+			await screen.findByText(new RegExp(products[0].name))
+		).toBeInTheDocument();
+		expect(
+			await screen.findByText(new RegExp(products[0].price.toString()))
+		).toBeInTheDocument();
+	});
+
+	test('should render message if product not found', async () => {
+		server.use(http.get('/products/1', () => HttpResponse.json(null)));
+
+		render(<ProductDetail productId={1} />);
+
+		const message = await screen.findByText(/not found/i);
+		expect(message).toBeInTheDocument();
+	});
+
+	test('should render an error for invalid id', async () => {
+		render(<ProductDetail productId={0} />);
+
+		const error = await screen.findByText(/invalid/i);
+		expect(error).toBeInTheDocument();
+	});
 });
